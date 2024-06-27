4ff0533c463c0252229d181fb2ae23fe8feb148a
aliNassef <anassef798@gmail.com>
2024-06-28 01:10:01 +0300

    add validateor to sign up

--- lib/features/auth/sign_up/presentation/view_model/sign_up_cubit/sign_up_cubit.dart
+++ lib/features/auth/sign_up/presentation/view_model/sign_up_cubit/sign_up_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:fruit_hup/constants.dart';
 import 'package:fruit_hup/core/cache/cache_helper.dart';
 import 'package:fruit_hup/core/service_locator.dart';
@@ -11,7 +12,12 @@ class SignUpCubit extends Cubit<SignUpState> {
   SignUpCubit(this._signUpRepo) : super(SignUpInitial());
 
   final SignUpRepo _signUpRepo;
+  // validate input
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  // checkBox State
   bool checkedBox = false;
+  // sign up parametes
   TextEditingController name = TextEditingController();
   TextEditingController email = TextEditingController();
   TextEditingController pass = TextEditingController();
--- lib/features/auth/sign_up/presentation/views/widgets/sign_up_view_body.dart
+++ lib/features/auth/sign_up/presentation/views/widgets/sign_up_view_body.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hup/core/shared/functions/toast_dialog.dart';
 import 'package:fruit_hup/core/shared/widgets/default_app_button.dart';
-import 'package:fruit_hup/core/utils/app_colors.dart';
-import 'package:fruit_hup/core/utils/app_styles.dart';
 import 'package:fruit_hup/features/auth/sign_up/presentation/view_model/sign_up_cubit/sign_up_cubit.dart';
 import 'package:go_router/go_router.dart';
 import '../../../../../../core/shared/functions/build_loading_box.dart';
@@ -12,6 +10,7 @@ import '../../../../../../core/shared/widgets/custom_text_form_field.dart';
 import '../../../../../../core/shared/widgets/top_bar.dart';
 import '../../../../../../core/utils/app_router.dart';
 import '../../../../../../generated/l10n.dart';
+import '../../../../sign_in/presentation/views/widgets/donot_have_account.dart';
 import 'terms_and_condation_check_box.dart';
 
 class SignUpViewBody extends StatelessWidget {
@@ -39,67 +38,71 @@ class SignUpViewBody extends StatelessWidget {
       },
       builder: (context, state) => SingleChildScrollView(
         physics: BouncingScrollPhysics(),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            VerticalSpace(20),
-            TopBar(
-              onTap: () {
-                context.pop();
-              },
-              text: S.of(context).newaccount,
-            ),
-            VerticalSpace(24),
-            CustomTextFormField(
-              hintText: S.of(context).fullname,
-            ),
-            VerticalSpace(16),
-            CustomTextFormField(
-              controller: context.read<SignUpCubit>().email,
-              hintText: S.of(context).email,
-            ),
-            VerticalSpace(16),
-            CustomTextFormField(
-              controller: context.read<SignUpCubit>().pass,
-              hintText: S.of(context).password,
-              showIcon: true,
-            ),
-            VerticalSpace(16),
-            TermsAndConditionsCheckbox(),
-            VerticalSpace(30),
-            DefaultAppButton(
-              onPressed: () {
-                if (context.read<SignUpCubit>().checkedBox == true) {
-                  context.read<SignUpCubit>().signUpWithEmailAndPass();
-                }
-              },
-              text: S.of(context).signUp,
-            ),
-            VerticalSpace(30),
-            Text.rich(
-              textAlign: TextAlign.center,
-              TextSpan(
-                children: [
-                  TextSpan(
-                    text: S.of(context).haveAccount,
-                    style: AppStyles.textStyle16SB.copyWith(
-                      color: AppColors.gray400,
-                    ),
-                  ),
-                  TextSpan(
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        context.push(AppRouter.signInView);
-                      },
-                    text: S.of(context).login,
-                    style: AppStyles.textStyle16SB.copyWith(
-                      color: AppColors.mainColor,
-                    ),
-                  ),
-                ],
+        child: Form(
+          key: context.read<SignUpCubit>().formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              VerticalSpace(20),
+              TopBar(
+                onTap: () {
+                  context.pop();
+                },
+                text: S.of(context).newaccount,
               ),
-            ),
-          ],
+              VerticalSpace(24),
+              CustomTextFormField(
+                hintText: S.of(context).fullname,
+              ),
+              VerticalSpace(16),
+              CustomTextFormField(
+                validator: (val) {
+                  final regex = RegExp(r'^[a-zA-Z0-9._%+-]+@gmail\.com$');
+                  if (val == null || val.isEmpty) {
+                    return 'Please enter an email';
+                  } else if (!regex.hasMatch(val)) {
+                    return 'Please enter a valid Gmail address';
+                  }
+                  return null;
+                },
+                controller: context.read<SignUpCubit>().email,
+                hintText: S.of(context).email,
+              ),
+              VerticalSpace(16),
+              CustomTextFormField(
+                validator: (val) {
+                  if (val == null || val.isEmpty) {
+                    return 'Please enter a password';
+                  }
+                  return null;
+                },
+                controller: context.read<SignUpCubit>().pass,
+                hintText: S.of(context).password,
+                showIcon: true,
+              ),
+              VerticalSpace(16),
+              TermsAndConditionsCheckbox(),
+              VerticalSpace(30),
+              DefaultAppButton(
+                onPressed: () {
+                  if (context.read<SignUpCubit>().checkedBox == true) {
+                    if (context
+                        .read<SignUpCubit>()
+                        .formKey
+                        .currentState!
+                        .validate()) {
+                      context.read<SignUpCubit>().signUpWithEmailAndPass();
+                    }
+                  } else if (context.read<SignUpCubit>().checkedBox == false) {
+                    showToast(text: 'هل توافق على الشروط والاحكام');
+                  }
+                },
+                text: S.of(context).signUp,
+              ),
+              VerticalSpace(30),
+              DonotHaveAccount(),
+            ],
+          ),
         ),
       ),
     );
