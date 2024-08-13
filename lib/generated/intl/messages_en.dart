a0c61b4918f864ab1c4673ead392c7053a8d3544
aliNassef <anassef798@gmail.com>
2024-08-14 02:05:05 +0300

    ..

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -24,64 +24,23 @@ class MessageLookup extends MessageLookupByLibrary {
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
         "HaveAccount":
             MessageLookupByLibrary.simpleMessage(" تمتلك حساب بالفعل؟ "),
-        "ar": MessageLookupByLibrary.simpleMessage("العربية"),
         "bestSeller": MessageLookupByLibrary.simpleMessage("الأكثر مبيعًا"),
-        "cancel": MessageLookupByLibrary.simpleMessage("لا أرغب"),
-        "cart": MessageLookupByLibrary.simpleMessage("السلة"),
-        "changePass": MessageLookupByLibrary.simpleMessage("تغيير كلمة المرور"),
-        "chooseLanguage": MessageLookupByLibrary.simpleMessage("اختر اللغة"),
-        "confirm": MessageLookupByLibrary.simpleMessage("تأكيد"),
-        "confirmPass":
-            MessageLookupByLibrary.simpleMessage("تأكيد كلمة المرور الجديده"),
         "createNewAccount":
             MessageLookupByLibrary.simpleMessage("قم بانشاء حساب"),
-        "currentPass":
-            MessageLookupByLibrary.simpleMessage("كلمة المرور الحالي"),
-        "deleteAll": MessageLookupByLibrary.simpleMessage("حذف الكل"),
-        "doYOuWantToLogout":
-            MessageLookupByLibrary.simpleMessage("هل تريد تسجيل الخروج؟"),
         "email": MessageLookupByLibrary.simpleMessage("البريد الإلكتروني"),
-        "en": MessageLookupByLibrary.simpleMessage("English"),
-        "fav": MessageLookupByLibrary.simpleMessage("المفضلة"),
-        "favorites": MessageLookupByLibrary.simpleMessage("المفضلة"),
-        "filter": MessageLookupByLibrary.simpleMessage("تصنيف حسب :"),
         "forgetPass": MessageLookupByLibrary.simpleMessage("نسيت كلمة المرور؟"),
         "fromRules": MessageLookupByLibrary.simpleMessage(
             " من خلال إنشاء حساب ، فإنك توافق على "),
         "fullname": MessageLookupByLibrary.simpleMessage("الاسم كامل"),
         "goodMorning": MessageLookupByLibrary.simpleMessage("صباح الخير !.."),
         "haveAccount": MessageLookupByLibrary.simpleMessage("لا تمتلك حساب؟"),
-        "help": MessageLookupByLibrary.simpleMessage("المساعدة"),
-        "lang": MessageLookupByLibrary.simpleMessage("العربية"),
-        "language": MessageLookupByLibrary.simpleMessage("اللغة"),
-        "lastSearchInfo":
-            MessageLookupByLibrary.simpleMessage("عمليات البحث الأخيرة"),
         "login": MessageLookupByLibrary.simpleMessage("تسجيل دخول"),
         "lot": MessageLookupByLibrary.simpleMessage("المزيد"),
-        "main": MessageLookupByLibrary.simpleMessage("الرئيسية "),
-        "myOrders": MessageLookupByLibrary.simpleMessage("طلباتي"),
-        "myProfile": MessageLookupByLibrary.simpleMessage("حسابي"),
-        "newPass": MessageLookupByLibrary.simpleMessage("كلمة المرور الجديدة"),
         "newaccount": MessageLookupByLibrary.simpleMessage("حساب جديد"),
-        "notification": MessageLookupByLibrary.simpleMessage("الاشعارات"),
         "or": MessageLookupByLibrary.simpleMessage("أو"),
-        "ourProducts": MessageLookupByLibrary.simpleMessage("منتجاتنا"),
         "password": MessageLookupByLibrary.simpleMessage("كلمة المرور"),
-        "pay": MessageLookupByLibrary.simpleMessage("الدفع"),
-        "payments": MessageLookupByLibrary.simpleMessage("المدفوعات"),
-        "personalAccount": MessageLookupByLibrary.simpleMessage("الملف الشخصي"),
-        "personalInfo":
-            MessageLookupByLibrary.simpleMessage("المعلومات الشخصية"),
-        "priceRange": MessageLookupByLibrary.simpleMessage("السعر :"),
-        "productList": MessageLookupByLibrary.simpleMessage("المنتجات "),
-        "products": MessageLookupByLibrary.simpleMessage("المنتجات"),
-        "public": MessageLookupByLibrary.simpleMessage("عام"),
-        "result": MessageLookupByLibrary.simpleMessage("نتائج"),
-        "saveChanges": MessageLookupByLibrary.simpleMessage("حفظ التغييرات"),
         "search": MessageLookupByLibrary.simpleMessage("البحث"),
         "searchFor": MessageLookupByLibrary.simpleMessage("ابحث عن......."),
-        "shoppingCart": MessageLookupByLibrary.simpleMessage("سلة التسوق "),
-        "signOut": MessageLookupByLibrary.simpleMessage("تسجيل الخروج"),
         "signUp": MessageLookupByLibrary.simpleMessage("إنشاء حساب جديد"),
         "signwithapple":
             MessageLookupByLibrary.simpleMessage("تسجيل بواسطة أبل"),
@@ -89,13 +48,7 @@ class MessageLookup extends MessageLookupByLibrary {
             MessageLookupByLibrary.simpleMessage("تسجيل بواسطة فيسبوك"),
         "signwithgoogle":
             MessageLookupByLibrary.simpleMessage("تسجيل بواسطة جوجل"),
-        "sorryInfonotfound": MessageLookupByLibrary.simpleMessage(
-            "عفوًا... هذه المعلومات غير متوفرة للحظة"),
-        "success": MessageLookupByLibrary.simpleMessage("تم بنجاح !"),
         "termsAndCondation": MessageLookupByLibrary.simpleMessage(
-            " الشروط \nوالأحكام الخاصة بنا"),
-        "theme": MessageLookupByLibrary.simpleMessage("الوضع"),
-        "to": MessageLookupByLibrary.simpleMessage("الي"),
-        "whoAreWe": MessageLookupByLibrary.simpleMessage("من نحن")
+            " الشروط \nوالأحكام الخاصة بنا")
       };
 }
